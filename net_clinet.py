bc3176f1323e832211aa36ee56b6aa0f846a681d
sym <849228735@qq.com>
2017-03-25 02:17:22 +0800

    添加了简单的多账号功能
    另外减少了全局变量的使用

--- net_clinet.py
+++ net_clinet.py
@@ -1,4 +1,5 @@
 #python 3.5
+# -*- coding: utf-8 -*-
 
 import sys
 import http.client
@@ -12,9 +13,8 @@ target_port = 69
 path_portal = '/cgi-bin/srun_portal'
 path_info = '/cgi-bin/rad_user_info'
 
-usr_name = ''
+username = ''
 pwd = ''
-# TODO: read username from file
 
 profiles = {
     'accounts': [],
@@ -22,32 +22,29 @@ profiles = {
 
 }
 
-def add_accuont(usr_name, pwd):
-    account = {'username': usr_name, 'pwd': pwd}
+def require_account():
+    username = ''
+    pwd = ''
+    while len(username) == 0:
+        username = input('please input your username:\n>>')
+    while len(pwd) == 0:
+        pwd = input('please input your password:\n>>')
+    return (username, pwd)
+
+def add_accuont(username, pwd):
+    account = {'username': username, 'pwd': pwd}
     profiles['accounts'].append(account)
-    profiles['accounts_sel'] = len(profiles['accounts'])-1
+
+def set_selected(num):
+    if len(profiles['accounts']) > num:
+        profiles['accounts_sel'] = num
+    else:
+        print('set_selected: num out of range')
 
 
 profile_file_name = 'profiles.json'
 
-try:
-    f_prf = open(profile_file_name, 'r+')
-    print('loading profiles...')
-    try:
-        profiles = json.loads(f_prf.read())
-        usr_name = profiles['accounts'][profiles['accounts_sel']]['username']
-        pwd = profiles['accounts'][profiles['accounts_sel']]['pwd']
-    except Exception as e:
-        print('an error occurs when reading profiles file:', e)
-    
-except Exception as e:
-    print('no profiles file')
-    while len(usr_name) == 0:
-        usr_name = input('please input your username:\n')
-    while len(pwd) == 0:
-        pwd = input('please input your password:\n')
-    add_accuont(usr_name, pwd)
-
+def save_profile():
     try:
         f_prf = open(profile_file_name, 'w')
         f_prf.write(json.dumps(profiles, indent=4))
@@ -58,63 +55,113 @@ except Exception as e:
 
 
 
+try:
+    f_prf = open(profile_file_name, 'r+')
+    print('loading profiles...')
+    try:
+        profiles = json.loads(f_prf.read())
+        selected = profiles['accounts_sel']
+        if len(profiles['accounts']) < selected + 1:
+            raise 'selected accounts not exist'
+        username = profiles['accounts'][selected]['username']
+        pwd = profiles['accounts'][selected]['pwd']
+    except Exception as e:
+        print('an error occurs when reading profiles file:', e)
+    
+except Exception as e:
+    print('no profiles file')
+    (username, pwd) = require_account()
+    add_accuont(username, pwd)
+    save_profile()
+    
+
+
+
 def check_info():
-    conn = http.client.HTTPConnection(target_ip, target_port)
-    # connect
-    conn.request('GET', path_info)
-    resp = conn.getresponse()
-    # response
-    response_text = str(resp.read(), 'utf-8')
-    conn.close()
-    # return response_text
+    try:
+        conn = http.client.HTTPConnection(target_ip, target_port)
+        # connect
+        conn.request('GET', path_info)
+        resp = conn.getresponse()
+        # response
+        response_text = str(resp.read(), 'utf-8')
+        conn.close()
+        # return response_text
 
-    if response_text == 'not_online':
-        # not online
-        return False
-    else:
-        # parse response
-        splited_text = response_text.split(',')
-        online_info = {
-            'username': splited_text[0],
-            'login_timestamp': splited_text[1],
-            # time stamp is in second
-            'data_usage_curr_login': splited_text[3],
-            'total_data_usage': splited_text[6],
-            'ip': splited_text[8],
-            'balance': splited_text[11],
+        if response_text == 'not_online':
+            # not online
+            return False
+        else:
+            # parse response
+            splited_text = response_text.split(',')
+            online_info = {
+                'username': splited_text[0],
+                'login_timestamp': splited_text[1],
+                # time stamp is in second
+                'data_usage_curr_login': splited_text[3],
+                'total_data_usage': splited_text[6],
+                'ip': splited_text[8],
+                'balance': splited_text[11],
+            }
+            return online_info
+    except Exception as e:
+        print('connecting error:', e)
+
+
+def login(username, pwd):
+    try:
+        headers_login = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cache-Control': 'no-cache'
         }
-        return online_info
+        cont_login = 'username={}&password={}&ac_id=1&action=login&type=1&n=100'
+        req_body = cont_login.format(username, pwd)
 
+        conn = http.client.HTTPConnection(target_ip, target_port)
+        conn.request('POST', path_portal, req_body, headers_login)
+        resp = conn.getresponse()
+        response_text = str(resp.read(), 'utf-8')
+        conn.close()
+        return response_text
+    except Exception as e:
+        print('connecting error:', e)
 
-def login(usr_name, pwd):
-    headers_login = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cache-Control': 'no-cache'
-    }
-    cont_login = 'username={}&password={}&ac_id=1&action=login&type=1&n=100'
-    req_body = cont_login.format(usr_name, pwd)
+def login_as(*num):
+    if len(num) == 0:
+        sel = profiles['accounts_sel']
+    else:
+        sel = num[0]
 
-    conn = http.client.HTTPConnection(target_ip, target_port)
-    conn.request('POST', path_portal, req_body, headers_login)
-    resp = conn.getresponse()
-    response_text = str(resp.read(), 'utf-8')
-    conn.close()
-    return response_text
+    if len(profiles['accounts']) > sel:
+        username = profiles['accounts'][sel]['username']
+        pwd = profiles['accounts'][sel]['pwd']
+        return login(username, pwd)
+    else:
+        print('error in login_as(), num out of range')
+        return 'Error: number out of range'
 
-def logout(usr_name):
-    headers_logout = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cache-Control': 'no-cache'
-    }
-    cont_logout = 'action=logout&ac_id=1&username={}&type=2'
-    req_body = cont_logout.format(usr_name)
+def logout(online_info):
+    if 'username' in online_info:
+        username = online_info['username']
+        try:
+            headers_logout = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cache-Control': 'no-cache'
+            }
+            cont_logout = 'action=logout&ac_id=1&username={}&type=2'
+            req_body = cont_logout.format(username)
 
-    conn = http.client.HTTPConnection(target_ip, target_port)
-    conn.request('POST', path_portal, req_body, headers_logout)
-    resp = conn.getresponse()
-    response_text = str(resp.read(), 'utf-8')
-    conn.close()
-    return response_text
+            conn = http.client.HTTPConnection(target_ip, target_port)
+            conn.request('POST', path_portal, req_body, headers_logout)
+            resp = conn.getresponse()
+            response_text = str(resp.read(), 'utf-8')
+            conn.close()
+            return response_text
+        except Exception as e:
+            print('connecting error:', e)
+            return False
+    else:
+        return 'not online'
 
 def data_unit(in_byte):
     if isinstance(in_byte, str):
@@ -155,20 +202,37 @@ while True:
         print('not online')
         print()
 
-    op = input('input "login" to log in, "logout" to log out, "e" to exit program, others to check infomation:\n')
+    for account in enumerate(profiles['accounts']):
+        print('account {}: {}'.format(account[0], account[1]['username']))
+
+    op = re.findall('\w+',
+        input('input\naccount number to log in,\n"\
+logout" to log out,\n\
+"e" to exit program,\n\
+"a" to append new account,\n\
+others to check infomation:\n>>')
+    )
+
+    if len(op) == 0:
+        continue
 
     
-    if op in ('e', 'q', 'exit', 'quit'):
+    if op[0] in ('e', 'q', 'exit', 'quit'):
         print('exit...')
         break
 
-    elif op in ('login', 'log in', 'log on'):
+    elif op[0] in ('l', 'login', 'log in', 'log on') or re.match('\d+', op[0]):
         if online_info:
             print('you have already logged in')
         else:
-            print('loging in...')
+            m = re.match('\d+', op[0])
+            if m:
+                sel = int(m.group(0))
+            else:
+                sel = profiles['accounts_sel']
+            print('loging in as {}...'.format(profiles['accounts'][sel]['username']))
+            response_text = login_as(sel)
 
-            response_text = login(usr_name, pwd)
             print(response_text)
             
             if response_text.startswith('login_ok'):
@@ -179,16 +243,26 @@ while True:
                     print('Error:', m.groups()[0])
                 else:
                     print('Unknown error')
-    elif op in ('logout', 'log out'):
+
+    elif op[0] in ('logout', 'log out'):
         if online_info:
             print('logging out...')
 
-            response_text = logout(usr_name)
+            response_text = logout(online_info)
             if response_text.startswith('logout_ok'):
                 print('succeeded logging out')
             else:
-                print('failing logging out')
+                print('failing logging out,', response_text)
         else:
             print('not online')
+
+    elif op[0] in ('a'):
+        if len(op) < 3:
+            account = require_account()
+            add_accuont(account[0], account[1])
+            save_profile()
+        else:
+            add_accuont(op[1], op[2])
+            save_profile()
     else:
         pass
